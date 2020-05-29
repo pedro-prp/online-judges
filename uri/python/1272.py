f14d3e5e5d03214933b4f387647437634c545eda
Pedro Rodrigues <peddroprp@gmail.com>
2020-05-29 08:06:09 -0300

    concise 1272.py

--- uri/python/1272.py
+++ uri/python/1272.py
@@ -1,8 +1,5 @@
 n = int(input())
 
 for i in range(n):
-    s = input().split()
-    m = [word[0] for word in s]
-
-    print(''.join(m))
+    print(''.join([word[0] for word in input().split()]))
 
